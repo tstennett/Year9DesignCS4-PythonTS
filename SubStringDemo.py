2d82d44122f32d661799c7c16132fcec4dac9424
Tyler Stennett <tyler.stennett@10465.local>
2018-09-18 14:36:15 -0400

    Update

---
+++ SubStringDemo.py
@@ -0,0 +1,15 @@
+#This program will show how to effictively use substring
+#Prompt the User for their first name and their last name
+
+#Input
+
+fName = input("What is your first name: ")
+lName = input("What is your last name: ")
+
+#Process
+
+result = fName +  lName
+#result = fName[:3] + lName[1:3] <--- Code Name Code
+
+#Output
+print("Hi "+result)
