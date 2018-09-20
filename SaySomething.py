e4be5a7e27771e07150fefc7ce76d6324007479b
Tyler Stennett <tyler.stennett@10465.local>
2018-09-20 11:16:31 -0400

    Update

---
+++ SaySomething.py
@@ -0,0 +1,12 @@
+import os
+
+#Loop structure
+#Two types of loops:
+
+#Counted Loops
+
+#Conditional Loops
+
+while(True):
+	statement = input("What would you like me to say: ")
+	os.system("say "+statement)
---
+++ SaySomethingGUI.py
@@ -0,0 +1,19 @@
+import tkinter as tk 
+
+#The first thing you have to do is create main window
+#Mr. Miskew always calls this root
+
+root = tk.Tk() #This calls the constructor Tk() and builds your window
+
+
+
+
+
+
+
+
+root.mainloop() #This displays your root window and causes the program to wait
+				#This is EVENT DRIVEN PROGRAMMING
+
+print("End Program")
+			
