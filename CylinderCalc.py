8991d49baa7b67899cc0ea3a43979e8d39d5f30e
Tyler Stennett <tyler.stennett@10465.local>
2018-09-26 10:29:31 -0400

    Cylinder GUI

---
+++ CylinderCalc.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+
+def submit():
+	#does something once submit button is pressed
+	print("Submit pressed")
+	r = float(entr.get()) #gets what's in the entry box and makes it a float
+	h = float(enth.get())
+
+	v = math.pi*r*r*h
+	v = round(v,3) #rounds volume to 3 decimal places
+
+	output.insert(tk.INSERT, v) #inserts it in location
+
+root = tk.Tk()
+root.title("Volume of a Cylinder")
+
+labr = tk.Label(root, text="Radius") #makes labr a label
+labr.pack() #constructs then packs
+
+entr = tk.Entry(root) #an entry that takes whatever is typed in it
+entr.pack()
+
+labh = tk.Label(root, text="Height")  #label
+labh.pack()
+
+enth = tk.Entry(root)
+enth.pack()
+
+btn = tk.Button(root, text="Submit", command=submit)
+btn.pack()
+
+output = tk.Text(root, width=20, height=15, borderwidth=4, relief=tk.GROOVE)
+output.config(state="disabled")
+output.pack()
+
+
+root.mainloop()
+
---
+++ TestAnnoyingProgram.py
@@ -0,0 +1,13 @@
+import os
+while(1==1):
+	print("hi")
+	
+user_input = input()
+if user_input.lower() == "end":
+	break
+
+   
+		
+
+
+	
